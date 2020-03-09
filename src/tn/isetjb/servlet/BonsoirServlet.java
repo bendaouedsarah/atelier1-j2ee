cf5beb6969c3a49ab98027bf61472b10042abb95
ben_daoued_sarah <bendaouedsarah@gmail.com>
2020-03-09 12:55:16 +0100

    servlet

--- src/tn/isetjb/servlet/BonsoirServlet.java
+++ src/tn/isetjb/servlet/BonsoirServlet.java
@@ -4,27 +4,28 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+@WebServlet("/bonsoir")
+public class BonsoirServlet extends HttpServlet {
 
-public class BonjourServlet extends HttpServlet{
-private int compteur=0;
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		resp.setContentType("text/html");
         resp.setCharacterEncoding("UTF-8");
         
         PrintWriter out = resp.getWriter();
-        this.compteur++;
         out.println("<!DOCTYPE html>");
         out.println("<html>");
         out.println("<head>");
         out.println("<meta charset=\"utf-8\" />");
-        out.println("<title>Bonjour Iset</title>");
+        out.println("<title>Bonsoir Iset</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<p>Cette Servlet a été accédée "+this.compteur+" fois.</p>");
+        out.println("<p>Bonsoir Iset Djerba!</p>");
         out.println("</body>");
         out.println("</html>");
 	}
@@ -36,4 +37,5 @@ private int compteur=0;
 	}
 
 	
+
 }
